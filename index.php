18d52daa59e786ea1689236551bfcedc6403660e
nitishaggarwal1 <nitish.agarwal1993@gmail.com>
2021-10-27 23:57:38 +1100

    final code with cmd passed args

--- index.php
+++ index.php
@@ -6,11 +6,13 @@ $result = curl_exec($ch);
 $obj = json_decode($result, true);
 asort($obj["entries"]);
 
-$enteredCategory = "Calendar";
-$limit = 5;
+unset($argv[0]);
+$category = $argv[1];
+$limit = $argv[2];
 $printLimit = 0;
+
 foreach ($obj["entries"] as $value) {
-	if($value["Category"] == $enteredCategory){
+	if($value["Category"] == $category){
 	    echo '<pre>', print_r(reset($value));
 		$printLimit++;
 		if($printLimit == $limit){
