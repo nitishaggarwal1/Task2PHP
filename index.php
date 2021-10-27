5394bc32c7870c933f5aaf414ee0184b0eb64e3e
nitishaggarwal1 <nitish.agarwal1993@gmail.com>
2021-10-27 23:52:10 +1100

    working code but without Cmd args

--- index.php
+++ index.php
@@ -4,9 +4,20 @@ $ch = curl_init();
 curl_setopt_array($ch, [CURLOPT_RETURNTRANSFER => 1,CURLOPT_URL => "https://api.publicapis.org/entries"]);
 $result = curl_exec($ch);
 $obj = json_decode($result, true);
-$entries = json_encode($obj["entries"]);
+asort($obj["entries"]);
 
-
-foreach (json_decode($entries, true) as $value) {
-  echo '<pre>', print_r(reset($value));
+$enteredCategory = "Calendar";
+$limit = 5;
+$printLimit = 0;
+foreach ($obj["entries"] as $value) {
+	if($value["Category"] == $enteredCategory){
+	    echo '<pre>', print_r(reset($value));
+		$printLimit++;
+		if($printLimit == $limit){
+			break;
+		}
+    }
+}
+if($printLimit == 0){
+	echo "No results";
 }
